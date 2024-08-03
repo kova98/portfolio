a773e8f3bc524bd9e89be45c671e5dfc4fda3ff0
Roko <rokokovac98@gmail.com>
2024-08-03 09:59:34 +0200

    Adjust styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
-import {GitHubLogoIcon, LinkedInLogoIcon} from "@radix-ui/react-icons";
-import ContactButton from "@/app/contact-button";
+import Image from 'next/image';
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons';
+import ContactButton from '@/app/contact-button';
 
 const Home = () => {
     return (
-        <div className="flex flex-col min-h-screen justify-center items-center p-4 xl:scale-100 lg:scale-100">
-            <div className="flex flex-col lg:flex-row lg:items-center lg:space-x-10 max-w-4xl w-full">
+        <div className="flex flex-col min-h-screen justify-center items-center p-4">
+            <div className="flex flex-col lg:flex-row lg:items-center lg:space-x-10 max-w-4xl ">
+                <div className={"flex-grow"}></div>
                 <div className="flex flex-col items-center lg:items-start lg:w-2/5 mb-10 lg:mb-0">
                     <div>
                         <h1 className="text-5xl lg:text-6xl font-extrabold tracking-tight text-center lg:text-left">
@@ -16,34 +17,36 @@ const Home = () => {
                         </h2>
                     </div>
 
-                    <p className="pb-4 text-center lg:text-left mt-10 text-xl">
+                    <p className="pb-4 text-center lg:text-left mt-10 text-xl text-foreground">
                         Hi, I’m Roko — a Software Developer with 5+ years of professional experience.
                         I love learning new things, building fun side projects, and writing about software development.
                     </p>
-                    <ContactButton className="mt-10 ml-4 scale-125"/>
+                    <ContactButton className="mt-10"/>
                 </div>
                 <div className="flex flex-col items-center lg:items-center lg:w-2/5">
-                    <div className="w-full relative pt-[100%]">
+                    <div className="relative w-64 h-64">
                         <Image
                             src="/roko.jpg"
                             alt="portrait"
+                            layout="fill"
                             objectFit="cover"
-                            fill
-                            className="w-full h-full top-0 left-0 object-cover rounded-full p-14"
+                            className="rounded-full"
                         />
                     </div>
-                    <div className="grid grid-cols-3 gap-4 justify-items-center">
-                        <a href={"https://github.com/kova98"} className={"hover:bg-gray-800 hover:rounded"}>
+                    <div className={"flex-grow"}></div>
+                    <div className="grid grid-cols-3 gap-4 justify-items-center pt-24">
+                        <a href="https://github.com/kova98" className="hover:bg-gray-800 hover:rounded">
                             <GitHubLogoIcon className="h-12 w-12"/>
                         </a>
-                        <a href={"https://www.linkedin.com/in/kova98/"} className="hover:bg-gray-800 hover:rounded">
-                            <LinkedInLogoIcon className={"h-12 w-12"}/>
+                        <a href="https://www.linkedin.com/in/kova98/" className="hover:bg-gray-800 hover:rounded">
+                            <LinkedInLogoIcon className="h-12 w-12"/>
                         </a>
-                        <a href={"https://medium.com/@kova98"} className={" hover:bg-gray-800 hover:rounded"}>
+                        <a href="https://medium.com/@kova98" className="hover:bg-gray-800 hover:rounded">
                             <Image src="/medium-white.svg" height={50} width={50} alt="medium logo"/>
                         </a>
                     </div>
                 </div>
+                <div className={"flex-grow"}></div>
             </div>
             {/* TODO: projects, blog, contact */}
         </div>
