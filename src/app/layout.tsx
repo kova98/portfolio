edb0a4d7b669753f4688b09d3eff29a79a29d171
Roko <rokokovac98@gmail.com>
2024-08-04 21:35:21 +0200

    Add PostHog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import dynamic from 'next/dynamic';
+import { PHProvider } from '@/components/providers/posthog-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,6 +11,10 @@ export const metadata: Metadata = {
   title: 'Roko Kovač',
   description: 'Roko Kovač - Software Developer',
 };
+
+const PostHogPageView = dynamic(() => import('./posthog-pageview'), {
+  ssr: false,
+});
 
 export default function RootLayout({
   children,
@@ -18,11 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </body>
+      <PHProvider>
+        <body className={inter.className}>
+          <PostHogPageView />
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
+      </PHProvider>
     </html>
   );
 }
